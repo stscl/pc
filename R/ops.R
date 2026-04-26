e0e7593d9c997066eb3d3b1b0c86ee4e43d80540
SpatLyu <lyu.geosocial@gmail.com>
2026-04-27 00:00:01 +0800

    implement optimal parameters search for pc r interface

--- R/ops.R
+++ R/ops.R
@@ -37,53 +37,30 @@
   if (is.null(lib)) lib = which(!(is.na(tv) | is.na(sv)))
   if (is.null(pred)) pred = lib
 
-  return(RcppPCops(tv, sv, lib, pred, E, tau, k, maximize, style, zero.tolerance,
-                   dist.metric, relative, weighted, threads, lower.parallel, h, NULL, terra::nrow(data)))
+  return(RcppPCops(tv, sv, lib, pred, E, tau, k, maximize, style, zero.tolerance, dist.metric, 
+                   relative, weighted, threads, lower.parallel, h, NULL, terra::nrow(data)))
 }
 
 #' Pattern Causality
 #'
-#' @note `pc` only supports numeric input data.
+#' @inheritParams pc
 #'
-#' @param data Observation data.
-#' @param target Integer of column indice for the target variable.
-#' @param source Integer of column indice for the source variable.
-#' @param libsizes (optional) Number of observations used.
-#' @param E (optional) Embedding dimensions.
-#' @param k (optional) Number of nearest neighbors used for projection.
-#' @param tau (optional) Step of lag.
-#' @param style (optional) Embedding style (`0` includes current state, `1` excludes it).
-#' @param lib (optional) Libraries indices.
-#' @param pred (optional) Predictions indices.
-#' @param boot (optional) Number of bootstraps to perform.
-#' @param random (optional) Whether to use random sampling.
-#' @param seed (optional) Random seed.
-#' @param dist.metric (optional) Distance measure to be used.
-#' @param zero.tolerance (optional) Maximum number of zeros tolerated in signature space.
-#' @param relative (optional) Whether to calculate relative changes in embedding.
-#' @param weighted (optional) Whether to weight causal strength.
-#' @param threads (optional) Number of threads used.
-#' @param lower.parallel (optional) Whether to use lower level of parallelism.
-#' @param verbose (optional) Whether to show the progress bar.
-#' @param nb (optional) Neighbours list.
-#' @param h (optional) Prediction horizon.
+#' 
 #'
-#' @return A list (if `libsizes` is `NULL`) or dataframe.
+#' @return A list.
 #'
 #' @export
-#' @name pc
-#' @aliases pc,data.frame-method
-#' @references
-#' Stavroglou, S.K., Pantelous, A.A., Stanley, H.E., Zuev, K.M., 2020. Unveiling causal interactions in complex systems. Proceedings of the National Academy of Sciences 117, 7599–7605.
+#' @name ops
+#' @aliases ops,data.frame-method
 #'
 #' @examples
 #' columbus = sf::read_sf(system.file("case/columbus.gpkg", package="spEDM"))
-#' pc::pc(columbus, 1, 3, E = 6, k = 8)
+#' pc::ops(columbus, 1, 3, E = 6, k = 8)
 #'
-methods::setMethod("pc", "data.frame", .pc_ts)
+methods::setMethod("ops", "data.frame", .ops_ts)
 
-#' @rdname pc
-methods::setMethod("pc", "sf", .pc_lattice)
+#' @rdname ops
+methods::setMethod("ops", "sf", .ops_lattice)
 
-#' @rdname pc
-methods::setMethod("pc", "SpatRaster", .pc_grid)
+#' @rdname ops
+methods::setMethod("ops", "SpatRaster", .ops_grid)
