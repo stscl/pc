fb96b40b89b191ebdf7d227e44a4ac665d30be00
SpatLyu <lyu.geosocial@gmail.com>
2026-05-01 00:00:01 +0800

    make per-sample causality output optional

--- src/PC.cpp
+++ src/PC.cpp
@@ -153,7 +153,7 @@ Rcpp::List RcppPC(
         static_cast<size_t>(std::abs(zero_tolerance)),
         static_cast<size_t>(std::abs(h)),
         dist_metric, relative, weighted,
-        static_cast<size_t>(std::abs(threads)));
+        static_cast<size_t>(std::abs(threads)), true);
 
     // --- Create DataFrame for per-sample causality ----------------------------
 
@@ -687,7 +687,7 @@ Rcpp::List RcppPCops(
         for (size_t kk : ks)
         {   
             if (kk < ee) continue;
-            
+
             for (size_t tt : taus)
                 unique_EkTau.emplace_back(ee, kk, tt);
 
