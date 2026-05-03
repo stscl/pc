e53d79c7a4d36a2186ce165108f18044480f1ff8
SpatLyu <lyu.geosocial@gmail.com>
2026-05-04 00:00:02 +0800

    enhance utility

--- R/internal_utility.R
+++ R/internal_utility.R
@@ -21,21 +21,20 @@
   }
   names(data) = c("target", "source")
 
-  mat = as.matrix(data)
-  if (!(typeof(mat) %in% c("integer", "double"))) {
+  if (!all(apply(data, 2, typeof) %in% c("integer", "double"))) {
     stop("Non-numeric values detected in input data. 
           Please remove columns such as dates, characters, or factors.", call. = FALSE)
   }
 
   res = vector("list", 2)
   if (is.null(coords)) {
-    res[[1]] = mat[, 1, drop = TRUE]
-    res[[2]] = mat[, 2, drop = TRUE]
+    res[[1]] = data[, 1, drop = TRUE]
+    res[[2]] = data[, 2, drop = TRUE]
   } else {
     data = as.data.frame(cbind(data, coords))
     res[[1]] = sdsfun::rm_lineartrend("target~x+y", data = data)
     res[[2]] = sdsfun::rm_lineartrend("source~x+y", data = data)
   }
-  
+
   return(res)
 }
