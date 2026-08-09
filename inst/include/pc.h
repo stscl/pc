87228e4f5c40608c31f0810c7c6931737da6fe57
SpatLyu <lyu.geosocial@gmail.com>
2026-08-10 00:00:01 +0800

    draft dmi with cpp source

--- inst/include/pc.h
+++ inst/include/pc.h
@@ -42,6 +42,7 @@
 #include "pc/numericutils.hpp"
 #include "pc/distance.hpp"
 #include "pc/neighbor.hpp"
+#include "pc/ksginfo.hpp"
 #include "pc/symdync.hpp"
 #include "pc/projection.hpp"
 #include "pc/patcaus.hpp"
