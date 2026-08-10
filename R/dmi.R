694e35ffac1f2502cdd45e880c0c4d7aab3cc2e5
SpatLyu <lyu.geosocial@gmail.com>
2026-08-11 00:00:01 +0800

    draft r interface for dmi

---
+++ R/dmi.R
@@ -0,0 +1,39 @@
+.dmi_ts = \(data, target, tau = 1:10, pred = NULL, k = 3,
+            base = exp(1), normalize = FALSE, threads = 1) {
+  tv = .validate_var(data, target)[[1]]
+  if (is.null(pred)) pred = which(!is.na(tv))
+  return(RcppFNN(tv, rt, eps, lib, pred, E, tau, style, dist.metric,
+                 k, threads, higher.parallel, NULL, NULL))
+}
+
+#' False Nearest Neighbors
+#'
+#' @param data Observation data.
+#' @param target Integer of column indice for the target variable.
+#' @param E (optional) Embedding dimensions.
+#' @param k (optional) Number of nearest neighbors used for evaluation.
+#' @param tau (optional) Step of lag.
+#' @param style (optional) Embedding style (`0` includes current state, `1` excludes it).
+#' @param lib (optional) Libraries indices.
+#' @param pred (optional) Predictions indices.
+#' @param dist.metric (optional) Distance measure to be used.
+#' @param rt (optional) Relative distance threshold.
+#' @param eps (optional) Absolute distance threshold.
+#' @param threads (optional) Number of threads used.
+#' @param higher.parallel (optional) Whether to use a higher level of parallelism.
+#' @param detrend (optional) Whether to remove the linear trend.
+#' @param nb (optional) Neighbours list.
+#' @param ... Additional arguments to absorb unused inputs in method dispatch.
+#'
+#' @return A vector.
+#' @export
+#' @name fnn
+#' @aliases fnn,data.frame-method
+#' @references
+#' Kennel, M.B., Brown, R., Abarbanel, H.D.I., 1992. Determining embedding dimension for phase-space reconstruction using a geometrical construction. Physical Review A 45, 3403–3411.
+#'
+#' @examples
+#' crash = sf::read_sf(system.file("case/crash.gpkg", package = "pc"))
+#' pc::fnn(crash, 1, threads = 1)
+#'
+methods::setMethod("dmi", "data.frame", .dmi_ts)
